bb3c5a03e5bdf7aa1bd820d0f721e61ccb66af47
Jose <jperez@redventures.com>
2017-03-26 11:50:02 -0400

    first fix

---
+++ RobinHood/rh/app/components/Home.js
@@ -0,0 +1,147 @@
+/* eslint-disable */
+// @flow
+import React, { Component } from 'react';
+// import { Link } from 'react-router';
+import styles from './Home.css';
+import _ from 'lodash';
+import Api from '../helpers/api';
+import PriceService from '../services/priceService';
+
+
+
+export default class Home extends Component {
+  constructor(props) {
+        super(props);
+
+        this.state = {
+            stockTradeInput: "",
+            stockTrade: "",
+            Stock: {
+                last_trade_price: ""
+            },
+            price: "",
+            totalUnsold: 0,
+            totalSold: 0,
+            totalBought: 0,
+            value: "",
+            userName: "",
+            password: "",
+            token: "",
+            isUserInfoSet: false,
+            accountNumber: "",
+        };
+        this.start = this.start.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleChangeUserName = this.handleChangeUserName.bind(this);
+        this.handleChangePassword = this.handleChangePassword.bind(this);
+        this.handleUserInfoSet = this.handleUserInfoSet.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+    
+    handleChangeUserName(event) {
+        this.setState({userName: event.target.value});
+    }
+
+    handleChangePassword(event) {
+        this.setState({password: event.target.value});
+    }
+
+    handleUserInfoSet() {
+        this.setState({isUserInfoSet: true});
+    }
+
+    start(){
+        var userToken = Api.getToken(this.state.userName, this.state.password);
+        userToken.then((data) => { 
+            this.setState({token: data.data});
+        }).then(() => {
+
+          Api.getAccountInfo(this.state.userName, this.state.password, this.state.token).then((data) => { this.setState({accountNumber: data[1]})});
+
+            Api.historicData(this.state.value.toUpperCase()).then((results) => {
+                console.log(this.state.accountNumber);
+                var stockHistoryData = results;
+                console.log(stockHistoryData);
+                console.log('yooo', this.state.accountNumber);
+                const stockAvg = 0.10;
+                const stockFivePercent = 0.007;
+                const stockBuyPrice = 0.09;
+                const stockSellPrice = 0.10;
+                const stockSellOut = 0.09;
+                const accountUserName = this.state.userName;
+                const accountPassword = this.state.password;
+                const userAccountNumber = this.state.userName;
+
+                console.log("stockAvg", stockAvg);
+                console.log("stockFivePercent", stockFivePercent);
+                console.log("stockBuyPrice", stockBuyPrice);
+                console.log("stockSellPrice", stockSellPrice);
+                console.log("stockSellOut", stockSellOut);
+                console.log("stockSellOut", accountUserName);
+                console.log("stockSellOut", accountPassword);
+                console.log("stockSellOut", userAccountNumber);
+
+                const newService = new PriceService(this.state.value.toUpperCase(), { 
+                    buyPrice: stockBuyPrice,
+                    fivePercent: stockFivePercent,
+                    sellOutPrice: stockSellOut,
+                    sellPrice: stockSellPrice,
+                    avg: stockAvg,
+                    userName: accountUserName,
+                    password: accountPassword,
+                    accountNumber: userAccountNumber,
+                });
+
+                newService.start();
+
+                setInterval(() => {
+                    this.setState({totalUnsold: newService.getNumUnsoldPurchases()});
+                }, 100);
+
+                setInterval(() => {
+                    this.setState({totalBought: newService.getNumBought()});
+                }, 100);
+
+                setInterval(() => {
+                    this.setState({totalSold: newService.getNumSold()});
+                }, 100);
+            });
+        });
+    }
+
+  render() {
+    if(!this.state.isUserInfoSet) {
+    return (
+      <div>
+        <div className={styles.container} data-tid="container">
+          <label>
+          Username:
+          <input type="text" value={this.state.userName} onChange={this.handleChangeUserName} />
+          Password:
+          <input type="text" value={this.state.password} onChange={this.handleChangePassword} />
+          </label>
+          <button type="button" onClick={this.handleUserInfoSet} > Start </button>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <div className={styles.container} data-tid="container">
+            <label>
+            Stock:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <button type="button" onClick={() => this.start() }> Start </button>
+            <p>Num current shares: {this.state.totalUnsold}</p>
+            <p>Num current shares bought: {this.state.totalBought}</p>
+            <p>Num current shares sold: {this.state.totalSold}</p>
+        </div>
+      </div>
+    );
+  }
+  }
+}
---
+++ RobinHood/rh/app/helpers/purchase.js
@@ -0,0 +1,44 @@
+/* eslint-disable */
+import Api from './api';
+
+const isEndState = function(state) {
+    if(state === 'filled' || state === 'rejected' || state === 'canceled' || state === 'failed') {
+        return true;
+    }
+    return false;
+}
+
+export default class Purchase {
+    constructor(info) {
+        this._info = info;
+        if(!isEndState(this._info.state)) {
+            //start up a timer that periodically checks the status of the order.
+            const updateStatus = () => {
+                Api.orderStatus(this._info.url).then((newInfo) => {
+                    if(!isEndState(newInfo.state)) {
+                        setTimeout(updateStatus, 2000);
+                    } else {
+                        this._info = newInfo;
+                    }
+                });
+            };
+        }
+    }  
+    state() {
+        return this._info.state;
+    }
+    quantity() {
+        return this._info.quantity;
+    }
+    price() {
+        return this._info.price;
+    }
+    side() {
+        return this._info.side;
+    }
+    cancelPurchase() {
+        if(this._info.state !== 'canceled') {
+            //code to cancel purchase
+        }
+    }
+}
